6fa0b54a7c87a3384eda9a492fe6e8e665ea7c48
artacone <artacone@gmail.com>
2022-01-24 20:04:29 +0300

    Add err_checks

--- philo/src/init.c
+++ philo/src/init.c
@@ -21,19 +21,22 @@ static int	init_input(int argc, char *argv[], t_input *in)
 {
 	if (argc < 5 || argc > 6)
 	{
+		printf("Error: wrong number of args.\n");
 		return (0);
 	}
-	in->n_philos = ft_atoi(argv[1]);
-	in->time_to_die = ft_atoi(argv[2]);
-	in->time_to_eat = ft_atoi(argv[3]);
-	in->time_to_sleep = ft_atoi(argv[4]);
+	in->n_philos = parse_arg(argv[1]);
+	in->time_to_die = parse_arg(argv[2]);
+	in->time_to_eat = parse_arg(argv[3]);
+	in->time_to_sleep = parse_arg(argv[4]);
 	if (argc == 5)
-	{
 		in->n_to_eat = -1;
-	}
 	else
+		in->n_to_eat = parse_arg(argv[5]);
+	if (in->n_philos * in->time_to_die * in->time_to_eat
+		* in->time_to_sleep * in->n_to_eat == 0)
 	{
-		in->n_to_eat = ft_atoi(argv[5]);
+		printf("Error: wrong arguments.\n");
+		return (0);
 	}
 	return (1);
 }
@@ -44,7 +47,7 @@ static int	init_mutexes(t_table *table)
 	pthread_mutex_t	*forks;
 
 	n = table->input.n_philos;
-	forks = malloc(n * sizeof *forks);
+	forks = malloc(n * sizeof(*forks));
 	if (forks == NULL)
 		return (0);
 	while (n > 0)
@@ -69,7 +72,7 @@ static int	init_philos(t_table *table)
 	t_philo	*philos;
 
 	n = table->input.n_philos;
-	philos = malloc(n * sizeof *philos);
+	philos = malloc(n * sizeof(*philos));
 	if (philos == NULL)
 		return (0);
 	i = 0;
@@ -94,7 +97,7 @@ static int	init_threads(t_table *table)
 {
 	pthread_t	*threads;
 
-	threads = malloc(table->input.n_philos * sizeof *threads);
+	threads = malloc(table->input.n_philos * sizeof(*threads));
 	if (threads == NULL)
 		return (0);
 	table->threads = threads;
